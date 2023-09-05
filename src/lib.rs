98609387b6ffbbc7f04485ff00271be0a4171acf
GaiSensei <dawnis.chow@cornell.edu>
2023-09-05 09:17:03 -0400

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,44 @@
 use nalgebra::Vector3;
 
+pub fn relu(z: &f32) -> f32 {
+    match *z > 0. {
+        true => *z,
+        false => 0.
+    }
+}
+
 pub struct Nn {
     pub syn: Vector3<f32>,
     pub ax: f32,
     pub tau: f32,
-    pub resist: f32,
+    pub refractory: f32,
+    pub thresh: f32,
     pub connect: String
 }
 
 impl Nn {
 
-    pub fn fwd(&self, input: Vector3<f32>) -> f32 {
-        input.dot(&self.syn)
+    fn fwd(&mut self, input: Vector3<f32>) {
+        let impulse: f32 = input.dot(&self.syn);
+        self.ax = self.ax * (-self.tau).exp() + impulse;
     }
 
-    pub fn bkwd(&self) {
-        println!("I'm going backwards mom!")
+    fn learn(&self) {
+        println!("I'm learning mom!")
     }
+
+    fn reset(&mut self) {
+        self.refractory = 2.
+    }
+
+    pub fn fire(&mut self, input: Vector3<f32>, time_delta: f32) -> f32 {
+        self.fwd(input);
+        self.refractory = self.refractory - time_delta;
+        if self.ax > self.thresh & self.refractory <= 0 {
+            self.learn();
+            relu(&self.ax)
+        } else { 0. }
+
+    }
+
 }
