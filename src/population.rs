1df8cad344391d57c68c6985bdd12cf0c2cf942e
dchow <dchow@box.com>
2023-10-14 18:29:57 -0400

    made unit tests for populationd deterministic by adding rng to config

--- src/population.rs
+++ src/population.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use rand::prelude::*;
 use rand::distributions::{Distribution, Uniform};
 use log::*;
 
@@ -9,8 +10,42 @@ use crate::{graph::NeuralNetwork, enecode::EneCode};
 ///
 ///
 
-trait FitnessFunction {
+trait FitnessEvaluation {
     fn fitness(&self, agent: &mut NeuralNetwork) -> f32;
+}
+
+///Configuration for Population to Set Hyperparameters
+struct PopulationConfig<F: FitnessEvaluation> {
+    evaluator: F, 
+    rng: Box<dyn RngCore>,
+    epoch_size: usize,
+    mutation_rate_scale_per_epoch: f32,
+    mutation_effect_scale_per_epoch: f32,
+}
+
+impl<F: FitnessEvaluation> PopulationConfig<F> {
+    pub fn new(evaluator: F, 
+               epoch_size: usize, 
+               mutation_rate_scale_per_epoch: f32, 
+               mutation_effect_scale_per_epoch: f32,
+               rng_seed: Option<u8>) -> Self {
+
+        let mut rng: Box<dyn RngCore> = match rng_seed {
+            Some(seedu8) => {
+                let seed = [seedu8; 32];
+                Box::new(StdRng::from_seed(seed))
+            }
+            None => Box::new(rand::thread_rng())
+        };
+
+        PopulationConfig {
+            evaluator,
+            rng,
+            epoch_size,
+            mutation_rate_scale_per_epoch,
+            mutation_effect_scale_per_epoch,
+        }
+    }
 }
 
 struct Population {
@@ -49,29 +84,28 @@ impl Population {
         }
     }
 
-    fn evaluate_fitness<T: FitnessFunction>(&mut self, f: &T) {
+    fn evaluate_fitness<T: FitnessEvaluation>(&mut self, f: &T) {
         let fitness_vector: Vec<f32> = self.agents.iter_mut().map(|x| f.fitness(x)).collect();
         self.agent_fitness = fitness_vector;
     }
 
-    fn selection(&self, n_select: usize) -> Vec<usize> {
+    fn selection<R: Rng>(&self, rng: &mut R, n_select: usize) -> Vec<usize> {
         let truncated_population = self.truncate_population();
-        self.stochastic_universal_sampling(truncated_population, n_select)
+        self.stochastic_universal_sampling(rng, truncated_population, n_select)
     }
 
     /// Implements stochastic universal sampling, an efficient algorithm related to Roulette Wheel
     /// Selection for evolutionary aglorithms
-    fn stochastic_universal_sampling(&self, sample: Vec<usize>, n_select: usize) -> Vec<usize> {
+    fn stochastic_universal_sampling<R: Rng>(&self, rng: &mut R, sample: Vec<usize>, n_select: usize) -> Vec<usize> {
         let sample_fitness: Vec<f32> = sample.iter().map(|&idx| self.agent_fitness[idx]).collect();
         let total_population_fitness: f32 = sample_fitness.iter().sum();
         let point_spacing = total_population_fitness / (n_select as f32);
-        let mut rng = rand::thread_rng();
         let u = Uniform::from(0_f32..point_spacing);
 
         let mut selection: Vec<usize>  = Vec::new();
 
         //start the roulette pointer at a point between 0 and the first spacing
-        let mut roulette_pointer = u.sample(&mut rng); 
+        let mut roulette_pointer = u.sample(rng); 
 
         for _p in 0..n_select {
             let mut idx: usize = 0;
@@ -100,12 +134,11 @@ impl Population {
 
     }
 
-    fn generate_offspring(&self, parental_ids: Vec<usize>) -> Vec<NeuralNetwork> {
+    fn generate_offspring<R: Rng>(&self, rng: &mut R, parental_ids: Vec<usize>) -> Vec<NeuralNetwork> {
         let mut offspring: Vec<NeuralNetwork> = Vec::new();
 
         // Given selected parents, mate in pairs until the population size is fulfilled
         let num_parents = parental_ids.len();
-        let mut rng = rand::thread_rng();
 
         let mate_attempt_limit = self.size as f32 * 1.5;
 
@@ -119,7 +152,7 @@ impl Population {
             let parent_1 = parental_ids[a1];
             let parent_2 = *partner_list[a2];
 
-            let offspring_nn = self.agents[parent_1].recombine_enecode(&mut rng, &self.agents[parent_2] );
+            let offspring_nn = self.agents[parent_1].recombine_enecode(rng, &self.agents[parent_2] );
 
             match offspring_nn {
                 Ok(nn) => offspring.push(nn),
@@ -136,17 +169,19 @@ impl Population {
         offspring
     }
 
-    pub fn evolve<T:FitnessFunction>(&mut self, fitness_eval: &T, iterations_max: usize, max_fitness_criterion: f32) {
+    pub fn evolve<F: FitnessEvaluation>(&mut self, pop_config: PopulationConfig<F>, iterations_max: usize, max_fitness_criterion: f32) {
+        let mut rng = pop_config.rng;
+
         //reset generation value
         self.generation = 0;
 
         while self.generation < iterations_max {
-            self.evaluate_fitness(fitness_eval);
+            self.evaluate_fitness(&pop_config.evaluator);
 
             // Select same population size, but use SUS to select according to fitness
-            let selection = self.selection(self.size);
+            let selection = self.selection(&mut rng, self.size);
 
-            let mut offspring = self.generate_offspring(selection);
+            let mut offspring = self.generate_offspring(&mut rng, selection);
 
             for agent in offspring.iter_mut() {
                 agent.mutate(self.mutation_rate, self.mutation_effect_sd);
@@ -162,10 +197,9 @@ impl Population {
                 break;
             }
 
-            //TODO: Create configuration struct to set how often/fast to decrease rates
-            if self.generation % 50 == 0 {
-                self.mutation_rate *= 0.50;
-                self.mutation_effect_sd *= 0.50;
+            if self.generation % pop_config.epoch_size == 0 {
+                self.mutation_rate *= pop_config.mutation_rate_scale_per_epoch;
+                self.mutation_effect_sd *= pop_config.mutation_effect_scale_per_epoch;
             }
 
             info!("Observing population fitness {} on generation {} with max of {}", self.population_fitness, self.generation, population_max);
@@ -195,7 +229,7 @@ mod tests {
         struct TestFitnessObject {
         }
 
-        impl FitnessFunction for TestFitnessObject {
+        impl FitnessEvaluation for TestFitnessObject {
             fn fitness(&self, _agent: &mut NeuralNetwork) -> f32 {
                 1.
             }
@@ -222,23 +256,29 @@ mod tests {
 
     #[test]
     fn test_stochastic_universal_sampling() {
+        let seed = [17; 32]; // Fixed seed for determinism
+        let mut rng = StdRng::from_seed(seed);
+
         let genome = GENOME_EXAMPLE.clone();
         let mut population_test = Population::new(genome, 3, 0.8, 0.1);
         population_test.agent_fitness = vec![5., 3., 2.];
         let sample: Vec<usize> = vec![0, 1, 2];
 
-        let sus: Vec<usize> = population_test.stochastic_universal_sampling(sample, 10);
+        let sus: Vec<usize> = population_test.stochastic_universal_sampling(&mut rng, sample, 10);
         assert_eq!(vec![0, 0, 0, 0, 0, 1, 1, 1, 2, 2], sus);
     }
 
 
     #[test]
     fn test_generate_offspring() {
+        let seed = [17; 32]; // Fixed seed for determinism
+        let mut rng = StdRng::from_seed(seed);
+
         let genome = GENOME_EXAMPLE.clone();
         let population_test = Population::new(genome, 10, 0.8, 0.1);
         let parent_id_vector = vec![0, 1, 3, 5];
 
-        let offspring_vec = population_test.generate_offspring(parent_id_vector);
+        let offspring_vec = population_test.generate_offspring(&mut rng, parent_id_vector);
 
         assert_eq!(offspring_vec.len(), 10);
     }
@@ -263,7 +303,7 @@ mod tests {
 
         }
 
-        impl FitnessFunction for XorEvaluation {
+        impl FitnessEvaluation for XorEvaluation {
             fn fitness(&self, agent: &mut NeuralNetwork) -> f32 {
                 let mut fitness_evaluation = self.fitness_begin;
 
@@ -283,10 +323,11 @@ mod tests {
                 fitness_evaluation
             }
         }
+        let ef = XorEvaluation::new();
+        
+        let config = PopulationConfig::new(ef, 50, 0.50, 0.50, Some(13));
 
-        let evaluation_function = XorEvaluation::new();
-
-        population.evolve(&evaluation_function, 200, 3.8);
+        population.evolve(config, 400, 3.8);
         assert!(population.population_fitness >= 3.7);
     }
 
