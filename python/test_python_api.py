5917d73f7a04e859354234098479cafffd3e9fab
dchow <dchow@box.com>
2024-02-01 10:16:13 -0500

    api builder is working in a Python test script

--- python/test_python_api.py
+++ python/test_python_api.py
@@ -1,10 +1,12 @@
 import evo_rl
 
 configuration = {
-        population_size: 200,
-        survival_rate: 0.2,
-        mutation_rate: 0.4, 
-        topology_mutation_rate: 0.4
+        "population_size": 200,
+        "survival_rate": 0.2,
+        "mutation_rate": 0.4, 
+        "topology_mutation_rate": 0.4
         }
 
-p = evo_rl.PopulationApi(
+p = evo_rl.PopulationApi(configuration)
+
+p.evolve()
