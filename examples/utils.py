8c7d8a660d2a6d5e774b6824228dbc76e2138330
dchow <dchow@box.com>
2024-06-29 12:22:54 -0400

    started dev on cartpole environment

--- examples/utils.py
+++ examples/utils.py
@@ -13,24 +13,48 @@ def visualize_gen(filename):
     output_path = source.render(filename=file_path, format='png')
     return file_path
 
-class MountainCarEnvironment:
+class GymnasiumEnv:
 
-    def __init__(self, environment, configuration): 
-        self.env = environment
-        self.evaluation_steps = 200
+    def __init__(self, environment_name, configuration): 
+        self.env_name = environment_name
+        self.env = gym.make(environment_name)
+        self.evaluation_steps = 999
+        self.observation_steps = 999
         self.configuration = configuration
 
+    def observe(self, population, idx):
+        self.env = gym.make(self.env_name, render_mode="human")
+        agent_idx = idx
+        observation, info = self.env.reset()
+
+        for _ in range(self.evaluation_steps):
+            population.agent_fwd(agent_idx, list(observation))
+            action = population.agent_out(agent_idx)
+            observation, reward, terminated, truncated, info = self.env.step(action)
+
+            if terminated or truncated:
+                observation, info = self.env.reset()
+
+        self.env.close()
+
+    def write_agent(self, population, agent_idx, file_save_path):
+        population.agent_checkpt(agent_idx, file_save_path)
+
+class CartPoleEnvironment(GymnasiumEnv):
+
+    def __init__(self, configuration): 
+        super().__init__("CartPole-v1", configuration)
 
     def evaluate_agent(self, population, agent_idx):
         observation, info = self.env.reset()
         fitness = 0
 
         for _ in range(self.evaluation_steps):
-            population.agent_fwd(agent_idx, [observation[0], observation[1]])
+            population.agent_fwd(agent_idx, list(observation))
             action = population.agent_out(agent_idx)
             observation, reward, terminated, truncated, info = self.env.step(action)
 
-            reward_delta = max([0, observation[1]]) + reward #sometimes there are NaNs, which crash the program
+            reward_delta = reward #sometimes there are NaNs, which crash the program
 
             if reward_delta == reward_delta:
                 fitness += reward_delta #forward progress + clipped reward penalty - complexity penalty
@@ -51,20 +75,38 @@ class MountainCarEnvironment:
             population.set_agent_fitness(agent_idx, fitness)
 
     
-    def observe(self, population, idx):
-        self.env = gym.make('MountainCarContinuous-v0', render_mode="human")
-        agent_idx = idx
+class MountainCarEnvironment(GymnasiumEnv):
+
+    def __init__(self, configuration): 
+        super().__init__("MountainCarContinuous-v0", configuration)
+
+    def evaluate_agent(self, population, agent_idx):
         observation, info = self.env.reset()
+        fitness = 0
 
         for _ in range(self.evaluation_steps):
-            population.agent_fwd(agent_idx, [observation[0], observation[1]]) # use random agent in population for now
+            population.agent_fwd(agent_idx, list(observation))
             action = population.agent_out(agent_idx)
             observation, reward, terminated, truncated, info = self.env.step(action)
 
+            reward_delta = max([0, observation[0]])*10 + reward #sometimes there are NaNs, which crash the program
+
+            if reward_delta == reward_delta:
+                fitness += reward_delta #forward progress + clipped reward penalty - complexity penalty
+
+            if fitness != fitness:
+                print(f"Error! Observing NaN Fitness")
+                
             if terminated or truncated:
                 observation, info = self.env.reset()
 
-        self.env.close()
+        #print(f"Setting agent {agent_idx} fitness to {fitness}")
 
-    def write_agent(self, population, agent_idx, file_save_path):
-        population.agent_checkpt(agent_idx, file_save_path)
+        fitness -= 0.1 * population.agent_complexity(agent_idx)
+        
+        if fitness < 0:
+            population.set_agent_fitness(agent_idx, random.uniform(0, 1))
+        else:
+            population.set_agent_fitness(agent_idx, fitness)
+
+    
