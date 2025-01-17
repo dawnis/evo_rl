a05c09c6aac5784bcecbff30744f5f17c17958e6
dchow <dchow@box.com>
2025-01-17 11:02:21 -0500

    working on post/fetch implementations

--- src/agent_wrapper.rs
+++ src/agent_wrapper.rs
@@ -2,12 +2,17 @@
 //! Python
 use crate::graph::NeuralNetwork;
 use crate::enecode::EneCode;
+
+use reqwest::Client;
+
 use pyo3::prelude::*;
+
 use std::path::PathBuf;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufWriter;
 use std::io::Result as FileResult;
+use std::error::Error;
 
 pub trait NnInputVector: Send {
     fn into_vec_f32(&self) -> Vec<f32>;
@@ -60,6 +65,22 @@ impl Agent {
         writer.write_all(serialized_genome.as_bytes())?;
         writer.flush()?;
         Ok(())
+    }
+
+    pub fn post_genome(&self, full_api_endpoint: &str) -> Result<String, Box<dyn Error>> {
+        let r_client = Client::new();
+        let serialized_genome = self.nn.serialize_genome();
+
+        let response = r_client.post(full_api_endpoint)
+            .json(&serialized_genome)
+            .send()?;        
+
+        if response.status().is_success() {
+            let body = response.text()?;
+            Ok(body)
+        } else {
+            Err(format!("Request failed with status: {}", response.status()).into())
+        }
     }
 }
 
