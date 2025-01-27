0eb91c712fd7fb11e5ca085cc9503ff20cdf2107
dchow <dchow@box.com>
2025-01-27 09:53:06 -0500

    fetch genome function draft 1

--- src/qdmanager.rs
+++ src/qdmanager.rs
@@ -6,6 +6,7 @@ use std::error::Error;
 use std::sync::Arc;
 use std::collections::HashMap;
 
+use crate::agent_wrapper::Agent;
 use crate::enecode::EneCode;
 
 
@@ -17,29 +18,55 @@ pub struct QDManager {
 
 }
 
+async fn fetch_genome(full_api_endpoint: &str, module: &str, location: (i32, i32)) -> Result<EneCode, Box<dyn Error>> {
+
+        let r_client = Client::new();
+    
+        let response = r_client
+            .get(full_api_endpoint)
+            .query(&[("module", module), ("param_x", &location.0.to_string()), ("param_y", &location.1.to_string())])
+            .send()
+            .await?;
+    
+        if response.status().is_success() {
+            let genome: EneCode = response.json().await?;
+            Ok(genome)
+        } else {
+            Err(format!("Request failed with status: {}", response.status()).into())
+        }
+}
+
+
 impl QDManager {
 
     pub fn new(module: Arc<str>, endpoint: Arc<str>) -> Self {
         QDManager {
             module,
-            endpoint
+            endpoint,
+            library: HashMap::new(),
         }
     }
 
-    pub fn post_genome(&self, full_api_endpoint: &str) -> Result<String, Box<dyn Error>> {
-        let r_client = Client::new();
-        let serialized_genome = self.nn.serialize_genome();
-
-        let response = r_client
-            .post(full_api_endpoint)
-            .json(&serialized_genome)
-            .send()?;
-
-        if response.status().is_success() {
-            let body = response.text()?;
-            Ok(body)
-        } else {
-            Err(format!("Request failed with status: {}", response.status()).into())
-        }
+    pub fn poplation_vector(population_size: usize) -> Vec<Agent> {
+        //TODO: for now assume that the module has entries in the database. If it is new,
+        //will deal with this later
     }
+
+
+    // pub fn post_genome(&self, full_api_endpoint: &str) -> Result<String, Box<dyn Error>> {
+    //     let r_client = Client::new();
+    //     let serialized_genome = self.nn.serialize_genome();
+    //
+    //     let response = r_client
+    //         .post(full_api_endpoint)
+    //         .json(&serialized_genome)
+    //         .send()?;
+    //
+    //     if response.status().is_success() {
+    //         let body = response.text()?;
+    //         Ok(body)
+    //     } else {
+    //         Err(format!("Request failed with status: {}", response.status()).into())
+    //     }
+    // }
 }
